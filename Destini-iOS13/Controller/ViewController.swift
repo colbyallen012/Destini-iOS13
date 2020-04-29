b0bbac9e582e2939e509d26b9e3f95d02f585d23
Colby Allen <colbyallen012@gmail.com>
2020-04-29 11:38:48 -0500

    add additional stories into array, display first story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,16 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    var storyBrain = StoryBrain()
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        storyLabel.text = storyBrain.getStoryText()
+//        choice1Button.setTitle(choice1, for: .normal)
+//        choice2Button.setTitle(choice2, for: .normal)
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
